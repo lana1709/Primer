0402ed2ea162967bac2ce81845e22faf2bcb9808
lana <user7@r.r>
2021-06-04 14:55:46 +0300

    Initial Commit

---
+++ PanelAvtorizaci/PanelAvtorizaciApp.swift
@@ -0,0 +1,17 @@
+//
+//  PanelAvtorizaciApp.swift
+//  PanelAvtorizaci
+//
+//  Created by user on 04.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct PanelAvtorizaciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
